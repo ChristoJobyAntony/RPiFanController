4a7b50abe914e237f924ece2aacc350e283e867f
Christo Joby Antony <christotantony2003@gmail.com>
2021-09-21 00:15:18 +0400

    Added basic documentation

--- src/data.rs
+++ src/data.rs
@@ -2,28 +2,28 @@
 pub struct FanSpeed {
     steps : Vec<SpeedStep>,
     min_duty_cycle : f32,
-    min_treshold : f32,
-    max_treshold : f32,
+    min_threshold : f32,
+    max_threshold : f32,
 }
 
 impl FanSpeed {
 
-    pub fn new(min_duty_cycle:f32, min_treshold:f32, max_treshold:f32) -> FanSpeed{
+    pub fn new(min_duty_cycle:f32, min_threshold:f32, max_threshold:f32) -> FanSpeed{
 
-        if min_treshold > max_treshold {panic!("Incorrectly configure Fanspeed !, Minimum fan threshold  greater than maximum treshold ")}
-        else if min_duty_cycle < 0f32 || min_duty_cycle > 1f32 {panic!("Incorrectly configure fanspeed ! Minimum Duty cycle is not in the valid range (0-1) ")}
+        if min_threshold > max_threshold {panic!("Incorrectly configure FanSpeed !, Minimum fan threshold  greater than maximum threshold ")}
+        else if min_duty_cycle < 0f32 || min_duty_cycle > 1f32 {panic!("Incorrectly configure FanSpeed ! Minimum Duty cycle is not in the valid range (0-1) ")}
         let steps =  Vec::new();
 
-        let mut f = FanSpeed {steps, min_duty_cycle, min_treshold, max_treshold};
+        let mut f = FanSpeed {steps, min_duty_cycle, min_threshold, max_threshold};
         
         f.steps.push(SpeedStep{
-            temperature:max_treshold,
+            temperature:max_threshold,
             fan_speed:1.0f32
         });
 
         f.steps.push(
             SpeedStep{
-                temperature:min_treshold,
+                temperature:min_threshold,
                 fan_speed:min_duty_cycle
             }
         );
@@ -32,8 +32,8 @@ impl FanSpeed {
     }
 
     pub fn push(&mut self, s : SpeedStep) {
-        if s.temperature > self.min_treshold || s.temperature > self.max_treshold {panic!("Temperarture is out of fan threshold range !")}
-        if s.fan_speed < self.min_duty_cycle || s.fan_speed > 100f32 {panic!("Fan Duty Cycel is out of range !")}
+        if s.temperature > self.min_threshold || s.temperature > self.max_threshold {panic!("Temperature is out of fan threshold range !")}
+        if s.fan_speed < self.min_duty_cycle || s.fan_speed > 100f32 {panic!("Fan Duty Cycle is out of range !")}
         let mut index :usize = 0; 
         for (i, step) in self.steps.iter().enumerate() {
             if s.temperature > step.temperature { index = i; break; }
@@ -43,8 +43,8 @@ impl FanSpeed {
 
     pub fn get_speed(&self, temp: f32) -> f32 {
         let mut speed = 0f32;
-        if temp < self.min_treshold {speed = 0f32}
-        else if temp >= self.max_treshold {speed = 1.0f32}
+        if temp < self.min_threshold {speed = 0f32}
+        else if temp >= self.max_threshold {speed = 1.0f32}
         else {
             for (i, low_step) in self.steps.iter().enumerate() {
                 if temp < low_step.temperature {
@@ -68,7 +68,7 @@ impl FanSpeed {
 
 impl SpeedStep {
     pub fn new (temperature:f32, fan_speed:f32) -> SpeedStep{
-        if fan_speed > 1f32 || fan_speed < 0f32 { panic!("Fanspeed shoudl ber between 0-1")}
+        if fan_speed > 1f32 || fan_speed < 0f32 { panic!("FanSpeed should ber between 0-1")}
         else if  temperature<0f32 || temperature>100f32 {panic!("Temperature should be between 0-100")};
         SpeedStep{fan_speed, temperature}
     }
--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,19 @@ mod data;
 
 pub use data::{FanSpeed, SpeedStep};
 
+// The temperature at which the fan should stop
 const OFF_THRESHOLD : f32  = 45f32;
+// The temperature at which the fan should spin at maximum speed
 const ON_THRESHOLD : f32 = 60f32;
+// Tune this value to reduce the clicking sound of the fan
 const MIN_DUTY_CYCLE : f32 = 0.50f32;
-const SENSITVITY :f32 = 1f32;
+// The delta in temperature, to initiate an update in fan speed
+const SENSITIVITY :f32 = 1f32;
+// The interval at which the temperature should be updated (seconds)
 const INTERVAL : u64 = 5;
 
 
-
+// Get CPU temperature from from RPi
 fn get_temp() -> f32 {
     let mut temp_text = fs::read_to_string("/sys/class/thermal/thermal_zone0/temp").unwrap();
     temp_text.truncate(5);
@@ -29,7 +34,8 @@ fn get_temp() -> f32 {
 fn main() {
 
     let running = Arc::new(AtomicBool::new(true));
-
+    
+    // Clean up the handlers when the program is closed unexpectedly 
     simple_signal::set_handler(
         &[Signal::Int, Signal::Term],
         {
@@ -38,8 +44,10 @@ fn main() {
         }
     );
 
-    let fan_speed = FanSpeed::new(MIN_DUTY_CYCLE, OFF_THRESHOLD, ON_THRESHOLD);
-    // fan_speed.push(SpeedStep::new(50f32,0.6f32));
+    // Create the FanSpeed Object based on the constants
+    let mut fan_speed = FanSpeed::new(MIN_DUTY_CYCLE, OFF_THRESHOLD, ON_THRESHOLD);
+    // Add steps to the FanSpeed Curve for more precise tuning
+    fan_speed.push(SpeedStep::new(50f32,0.6f32));
 
     let pwm = Pwm::with_frequency(Channel::Pwm0, 25f64, 0f64, Polarity::Normal, true).unwrap();
 
@@ -52,7 +60,8 @@ fn main() {
         
         new_temp = get_temp();
         
-        if (new_temp - old_temp).abs() > SENSITVITY {
+        // Logic to update fan speed
+        if (new_temp - old_temp).abs() > SENSITIVITY {
             new_speed = fan_speed.get_speed(new_temp);
 
             if (new_speed - old_speed).abs() > 0.01f32  {
